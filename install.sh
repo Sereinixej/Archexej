767633204b0a6d5755667b12da6cd2ad6979a375
Sereinixej <sereinia@edu.htl-villach.at>
2021-11-08 12:46:07 +0100

    dev

---
+++ install.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+#--------------------------------------
+#      A       R R R     CCCC   H   H
+#     A A      R   R    C       H   H
+#    A   A     RRRR     C       H H H
+#   A A A A    R   R    C       H   H
+#  A       A   R    R    CCCC   H   H
+#--------------------------------------
+
+## load install config
+source config.sh
+
+# mirrors
+echo "-------------------------------------------------"
+echo "Setting up mirrors for optimal download          "
+echo "-------------------------------------------------"
+iso=$(curl -4 ifconfig.co/country-iso)
+timedatectl set-ntp true
+pacman -S --noconfirm pacman-contrib terminus-font
+setfont ter-v22b
+sed -i 's/^#Para/Para/' /etc/pacman.conf
+pacman -S --noconfirm reflector rsync
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+mkdir /mnt
+
+echo "-------------------------------------------------"
+echo "-------select your disk to format----------------"
+echo "-------------------------------------------------"
+lsblk
+echo "Please enter disk to work on: (example /dev/sda)"
+read DISK
+echo "THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK"
+read -p "are you sure you want to continue (Y/N):" formatdisk
+case $formatdisk in
+
+y|Y|yes|Yes|YES)
+
+echo "--------------------------------------"
+echo -e "\nFormatting disk...\n$HR"
+echo "--------------------------------------"
+# disk prep
+sgdisk -Z ${DISK} # zap all on disk
+#dd if=/dev/zero of=${DISK} bs=1M count=200 conv=fdatasync status=progress
+sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+
+# create partitions
+sgdisk -n 1:0:+1000M ${DISK} # partition 1 (UEFI SYS), default start block, 512MB
+sgdisk -n 2:0:0     ${DISK} # partition 2 (Root), default start, remaining
+
+# set partition types
+sgdisk -t 1:ef00 ${DISK}
+sgdisk -t 2:8300 ${DISK}
+
+# label partitions
+sgdisk -c 1:"UEFISYS" ${DISK}
+sgdisk -c 2:"ROOT" ${DISK}
+
+# make filesystems
+echo -e "\nCreating Filesystems...\n$HR"
+if [[ ${DISK} =~ "nvme" ]]; then
+mkfs.vfat -F32 -n "UEFISYS" "${DISK}p1"
+mkfs.ext4 -L "ROOT" "${DISK}p2" -f
+mount -t ext4 "${DISK}p2" /mnt
+else
+mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
+mkfs.ext4 -L "ROOT" "${DISK}2" -f
+mount -t ext4 "${DISK}2" /mnt
+fi
+umount /mnt
+;;
+*)
+echo "Rebooting in 3 Seconds ..." && sleep 1
+echo "Rebooting in 2 Seconds ..." && sleep 1
+echo "Rebooting in 1 Second ..." && sleep 1
+reboot now
+;;
+esac
+
+# mount target
+mount -t ext4 -o subvol=@ -L ROOT /mnt
+mkdir /mnt/boot
+mkdir /mnt/boot/efi
+mount -t vfat -L UEFISYS /mnt/boot/
+
+if ! grep -qs '/mnt' /proc/mounts; then
+    echo "Drive is not mounted can not continue"
+    echo "Rebooting in 3 Seconds ..." && sleep 1
+    echo "Rebooting in 2 Seconds ..." && sleep 1
+    echo "Rebooting in 1 Second ..." && sleep 1
+    reboot now
+fi
