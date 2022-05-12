77a0dbb51233f257c55f8b01b16fecd5d1c87c95
Sereinixej <sereinia@edu.htl-villach.at>
2022-05-12 13:18:51 +0200

    pkgs

--- 5_packages.sh
+++ 5_packages.sh
@@ -2,7 +2,7 @@ pacman -S bluetooth --noconfirm
 systemctl enable bluetooth
 systemctl start bluetooth
 pacman -S pipewire-pulse --noconfirm
-pkgs=( "flameshot" "optimus-manager" "libreoffice-fresh" "neofetch" "brave-bin" "visual-studio-code-bin" )
+pkgs=( "flameshot" "keepassxc" "optimus-manager" "libreoffice-fresh" "neofetch" "brave-bin" "visual-studio-code-bin" )
 for i in "${pkgs[@]}"
 do
 	echo "installing $i ..."
